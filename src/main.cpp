ca628e63fae7459a7170758e4981908e37939b47
LuraMoth <85266594+Luramoth@users.noreply.github.com>
2023-05-17 16:45:13 -0700

    create window

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,58 @@
-#include "Engine/engine.h"
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class app {
+public:
+    GLFWwindow* window;
+
+    void run() {
+        initWindow();
+        init();
+        mainLoop();
+        cleanup();
+    }
+private:
+    void initWindow() {
+        glfwInit();
+
+        // tell openGL not to use OpenGL
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        // then make the window not resizable
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        // create the window
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Alcheminer", nullptr, nullptr);
+    }
+
+    void init() {
+
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
+};
+
 int main() {
+    app App{};
 
     try {
-        Engine::engine::run();
+        App.run();
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
