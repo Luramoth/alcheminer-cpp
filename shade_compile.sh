12cd2392208d69622f5c7e8fedf633490a634b5b
LuraMoth <85266594+Luramoth@users.noreply.github.com>
2023-05-18 19:42:27 -0700

    make compiled shaders go into build directory

--- shade_compile.sh
+++ shade_compile.sh
@@ -1,4 +1,4 @@
-ls
+mkdir "./cmake-build-debug/shaders/"
 
-glslc ./src/shaders/simple.vert -o ./src/shaders/simple.vert.spv
-glslc ./src/shaders/simple.frag -o ./src/shaders/simple.frag.spv
+glslc ./src/shaders/simple.vert -o ./cmake-build-debug/shaders/simple.vert.spv
+glslc ./src/shaders/simple.frag -o ./cmake-build-debug/shaders/simple.frag.spv
