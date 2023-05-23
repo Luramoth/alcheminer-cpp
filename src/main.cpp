a2fb7afab326b6fcfca653b6dc8a32f2eb0d9c5a
LuraMoth <85266594+Luramoth@users.noreply.github.com>
2023-05-23 01:12:13 -0700

    transition to useing Ogre

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,65 @@
-#include "app.h"
-
 #include <cstdlib>
 #include <iostream>
-#include <stdexcept>
+
+#include <Ogre.h>
+#include <OgreApplicationContext.h>
+#include <OgreInput.h>
+#include <OgreRTShaderSystem.h>
+
+class KeyHandler : public OgreBites::InputListener {
+    bool keyPressed(const OgreBites::KeyboardEvent& evt) override {
+        if (evt.keysym.sym == OgreBites::SDLK_ESCAPE) {
+            Ogre::Root::getSingleton().queueEndRendering();
+        }
+        return true;
+    }
+};
 
 int main() {
-    alchemy::app app{};
+    std::cout << "Hello World!\n";
 
-    try {
-        app.run();
-    } catch (const std::exception &e) {
-        std::cerr << e.what() << '\n';
-        return EXIT_FAILURE;
-    }
+    OgreBites::ApplicationContext ctx("Alcheminer");
+    ctx.initApp();
+
+    // get pointer to the already made root
+    Ogre::Root* root = ctx.getRoot();
+    Ogre::SceneManager* scnMgr = root->createSceneManager();
+
+    // register the scene with RTSS
+    Ogre::RTShader::ShaderGenerator* shaderGen =  Ogre::RTShader::ShaderGenerator::getSingletonPtr();
+    shaderGen->addSceneManager(scnMgr);
+
+    // let there be light
+    Ogre::Light* light = scnMgr->createLight("MainLight");
+    Ogre::SceneNode* lightNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+    lightNode->setPosition(0, 0, 15);
+    lightNode->attachObject(light);
+
+    // also need to tell Ogre where we are
+    Ogre::SceneNode* camNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+    camNode->setPosition(0, 0, 15);
+    camNode->lookAt(Ogre::Vector3(0, 0, -1), Ogre::Node::TS_PARENT);
+
+    // create the camera
+    Ogre::Camera* cam = scnMgr->createCamera("myCam");
+    cam->setNearClipDistance(5);
+    cam->setAutoAspectRatio(true);
+    camNode->attachObject(cam);
+
+    // tell it to render into the scene view
+    ctx.getRenderWindow()->addViewport(cam);
+
+    // add something to render
+    Ogre::Entity* ent = scnMgr->createEntity("Sinbad.mesh");
+    Ogre::SceneNode* node = scnMgr->getRootSceneNode()->createChildSceneNode();
+    node->attachObject(ent);
+
+    // reister for input events
+    KeyHandler keyHandler;
+    ctx.addInputListener(&keyHandler);
+
+    ctx.getRoot()->startRendering();
+    ctx.closeApp();
 
     return EXIT_SUCCESS;
 }
