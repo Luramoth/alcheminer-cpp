233aa9bb25d0c819574e38420383c05d12ab648c
LuraMoth <85266594+Luramoth@users.noreply.github.com>
2023-05-25 01:09:11 -0700

    add first person camera

--- src/main.cpp
+++ src/main.cpp
@@ -4,42 +4,44 @@
 #include <raylib.h>
 #include <raymath.h>
 
+#include "rlFPCamera.h"
+
 int main() {
     std::cout << "Hello World!\n";
+
+    SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT);
 
     int width = 800;
     int height = 450;
     InitWindow(width, height, "Alcheminer");
 
-    Camera3D camera;
-    camera.position = (Vector3){0, 10,10};
-    camera.target = (Vector3){0, 0, 0,};
-    camera.up = (Vector3){0, 1, 0};
-    camera.fovy = 45;
-    camera.projection = CAMERA_PERSPECTIVE;
+    rlFPCamera cam;
+    rlFPCameraInit(&cam, 45, (Vector3){0, 0, -10});
+    cam.MoveSpeed.z = 10;
+    cam.MoveSpeed.x = 5;
+
+    cam.FarPlane = 5000;
 
     Model cubeModel = LoadModelFromMesh(GenMeshCube(2, 2, 2));
     Vector3 cubePosition = {0, 0, 0};
 
-    SetCameraMode(camera, CAMERA_FREE);
-
-    SetTargetFPS(60);
-
     while (!WindowShouldClose()) {
-        UpdateCamera(&camera);
+        rlFPCameraUpdate(&cam);
 
         // rotate cube
-        cubeModel.transform = MatrixMultiply(cubeModel.transform, MatrixRotateY(DEG2RAD * 1));
+        cubeModel.transform = MatrixMultiply(cubeModel.transform, MatrixRotateY(DEG2RAD * 100 * GetFrameTime()));
 
         BeginDrawing();
         {
-                ClearBackground(RAYWHITE);
+            ClearBackground(RAYWHITE);
 
-                BeginMode3D(camera);
-                {
-                        DrawModel(cubeModel, cubePosition, 1, RED);
-                }
-                EndMode3D();
+            rlFPCameraBeginMode3D(&cam);
+            {
+                DrawModel(cubeModel, cubePosition, 1, RED);
+            }
+            rlFPCameraEndMode3D();
+
+            DrawFPS(10, 10);
         }
         EndDrawing();
     }
