7044ca9a200ba04d37f0b50979c5acf298823831
LuraMoth <85266594+Luramoth@users.noreply.github.com>
2023-06-10 13:07:47 -0700

    shader stuff

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,98 @@
 #include "include/glad/glad.h"
 #include <GLFW/glfw3.h>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+/////////////////////// data
+float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+};
+
+const char *vertexShaderSource = "#version 450 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
+
+const char *fragmentShaderSource = "#version 450\n"
+                                   "out vec4 FragColor;\n"
+                                   "\n"
+                                   "void main() {\n"
+                                   "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                   "}\0";
+
+/////////////////////// functions
+
+void framebuffer_size_callback(__attribute__((unused)) GLFWwindow* window, int width, int height){
     glViewport(0, 0, width, height);
 }
 
 void processInput(GLFWwindow *window){
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+}
+
+void graphicsInit(){
+    //// vertex buffer object
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    std::cout << "VBO ID: " << VBO << std::endl;
+
+    //// shaders
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    // check if shaders compiled
+    int  success;
+    char infoLog[512];
+
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    // vertex shader
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    // fragment shader
+    if(!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    //// shader program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // check for errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER_PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    // assuming nothing went wrong, continue
+    glUseProgram(shaderProgram);
+
+    // we don't need the shaders anymore since they are in the program so delete them
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 }
 
 ////////////////////////////////// MAIN FUNCTION //////////////////////////////////
@@ -23,8 +108,8 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Alcheminer", NULL, NULL);
-    if (window == NULL){
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Alcheminer", nullptr, nullptr);
+    if (window == nullptr){
         std::cout << "Failed to create window\n";
         glfwTerminate();
         return -1;
@@ -40,6 +125,8 @@ int main() {
     // viewport stuff
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);// when window is resized, tell openGL about it
+
+    graphicsInit();
 
     ////////////////////////////////// MAIN LOOP //////////////////////////////////
     while(!glfwWindowShouldClose(window)){
